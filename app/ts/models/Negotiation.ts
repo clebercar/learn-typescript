7900a54ca38c999c4fb5fa0abb06039a8e8817e7
clebercar <contatoclebercarvalho@gmail.com>
2020-01-05 23:15:59 -0300

    Criar entidade negociação

---
+++ app/ts/models/Negotiation.ts
@@ -0,0 +1,16 @@
+class Negotiation{
+  
+    constructor(private _date: Date, private _quantity: number, private _value: number) {}
+
+    get date(){
+        return this._date;
+    }
+
+    get quantity(){
+        return this._quantity;
+    }
+
+    get value(){
+        return this._value;
+    }
+}
