328c29b7faf624b240bb7f30f83adc9419459dd0
clebercar <contatoclebercarvalho@gmail.com>
2020-01-26 22:42:11 -0300

    Adicionar ações para atualizar os dados da tabela

--- app/ts/controllers/NegotiationController.ts
+++ app/ts/controllers/NegotiationController.ts
@@ -2,11 +2,15 @@ class NegotiationController {
     private _inputDate: HTMLInputElement;
     private _inputQuantity: HTMLInputElement;
     private _inputValue: HTMLInputElement;
+    private _negotiations = new Negotiations();
+    private _negotiationsView = new NegotiationsView('#negotiationsView');
 
     constructor() {
         this._inputDate = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantity = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValue = <HTMLInputElement>document.querySelector('#valor');
+
+        this._negotiationsView.update(this._negotiations);
     }
 
     add(event: Event){
@@ -17,7 +21,15 @@ class NegotiationController {
             parseInt(this._inputQuantity.value),
             parseFloat(this._inputValue.value)
         );
+        
+        this._negotiations.add(negotiation);
 
-        console.log(negotiation);
+        this._negotiations.toArray().forEach(negociation => {
+            console.log(negotiation.date)
+            console.log(negotiation.quantity)
+            console.log(negotiation.value)
+        })
+
+        this._negotiationsView.update(this._negotiations);
     }
 }
