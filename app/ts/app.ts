6763259ee16785527cb58e6b7f9846ddeee2eeb4
clebercar <contatoclebercarvalho@gmail.com>
2020-02-29 13:31:31 -0300

    Importar controller

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,5 @@
+import { NegotiationController } from './controllers/NegotiationController';
+
 const controller = new NegotiationController();
 // utilizando jquery
 $('.form').submit(controller.add.bind(controller))
