b84b9464d9927572c2b4d65b74fed3077f69e2b0
clebercar <contatoclebercarvalho@gmail.com>
2020-03-01 21:47:38 -0300

    Adicionar action para o botão de importação

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -3,3 +3,4 @@ import { NegotiationController } from './controllers/NegotiationController';
 const controller = new NegotiationController();
 // utilizando jquery
 $('.form').submit(controller.add.bind(controller))
+$('#botao-importar').click(controller.importData.bind(controller))
