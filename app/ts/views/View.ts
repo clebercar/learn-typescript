3787fc8c432726ce06992fa80ac54292cd868d0f
clebercar <contatoclebercarvalho@gmail.com>
2020-03-01 21:28:05 -0300

    Ajustar método

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,20 @@
 export abstract class View<T> {
     protected _element: JQuery;
+    private _escape: boolean;
 
-    constructor(selector: string) {
+    constructor(selector: string, escape = false) {
         this._element = $(selector);
+        this._escape = escape;
     }
 
-    update(model: T): void {
-        this._element.html(this.template(model));
+    update(model: T) {
+        let template = this.template(model);
+
+        if(this._escape) 
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
+        
+        this._element.html(template);
     }
 
-    abstract template(model: T): string
+    abstract template(model: T): string;
 }
